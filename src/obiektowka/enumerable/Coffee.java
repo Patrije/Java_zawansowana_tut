38c02dfb9f72be2433f7c351cc84ad28b3209846
patrycja <paatt10i@gmail.com>
2022-04-26 18:52:23 +0200

    enum przyklad

--- src/obiektowka/enumerable/Coffee.java
+++ src/obiektowka/enumerable/Coffee.java
@@ -1,7 +1,39 @@
 package obiektowka.enumerable;
 
 public enum Coffee {
-    LATTE,
-    ESPRESSO,
-    AMERICANO
+    LATTE(10, Roast.BLONDE),
+    ESPRESSO(0, Roast.DARK),
+    AMERICANO(5, Roast.MEDIUM);
+
+    private int milkLevel;
+    private Roast roastLevel;
+
+    Coffee(int milkLevel, Roast roastLevel){
+        this.milkLevel = milkLevel;
+        this.roastLevel = roastLevel;
+    }
+
+    public int getMilkLevel() {
+        return milkLevel;
+    }
+
+    public void setMilkLevel(int milkLevel) {
+        if(milkLevel > 0 && milkLevel <=10 ){
+        this.milkLevel = milkLevel;
+        }
+    }
+
+    public Roast getRoastLevel() {
+        return roastLevel;
+    }
+
+    public void setRoastLevel(Roast roastLevel) {
+        this.roastLevel = roastLevel;
+    }
+}
+
+enum Roast{
+    DARK,
+    MEDIUM,
+    BLONDE
 }
--- src/obiektowka/enumerable/EnumTest.java
+++ src/obiektowka/enumerable/EnumTest.java
@@ -5,12 +5,18 @@ public class EnumTest {
     public static void main(String[] args) {
         Coffee coffee = Coffee.LATTE;
         Coffee americano = Coffee.valueOf("AMERICANO");
-        System.out.println(americano);
+        System.out.println(americano.getMilkLevel());
 
         for (int i = 0; i < Coffee.values().length; i++) {
-            System.out.println(Coffee.values()[i]);
+            System.out.println("poziom wypalenia dla kawy o typie " +Coffee.values()[i]
+                    + " wynosi " + Coffee.values()[i].getRoastLevel());
         }
 
+        Coffee.LATTE.setRoastLevel(Roast.MEDIUM);
+        for (int i = 0; i < Coffee.values().length; i++) {
+            System.out.println("poziom wypalenia dla kawy o typie " +Coffee.values()[i]
+                    + " wynosi " + Coffee.values()[i].getRoastLevel());
+        }
         for (Coffee element: Coffee.values()) {
             System.out.println(element);
         }
@@ -23,7 +29,5 @@ public class EnumTest {
         if(coffee.equals(Coffee.AMERICANO)){
             System.out.println(" to kawa americano");
         }
-
-
     }
 }
