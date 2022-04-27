e9d0a4291749dc433b774847fa2ba20fd7e465fb
patrycja <paatt10i@gmail.com>
2022-04-27 19:24:42 +0200

    generyki zadanie

--- src/generyki/box/zadanie/Box.java
+++ src/generyki/box/zadanie/Box.java
@@ -2,7 +2,7 @@ package generyki.box.zadanie;
 
 public class Box {
 
-    public static  <T> void getFruit(T fruit){
+    public static <T> void getFruit(T fruit){
         System.out.println("In box is "+fruit);
     }
 }
